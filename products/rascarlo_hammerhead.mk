f8eedc86f8d0ed664072b858a78b8b9257c5452a
modstorm <daniel.gauci@gmail.com>
2014-08-25 10:45:03 -0700

    Update Adreno drivers

--- products/rascarlo_hammerhead.mk
+++ products/rascarlo_hammerhead.mk
@@ -23,3 +23,25 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 # boot animation
 PRODUCT_COPY_FILES += \
     vendor/rascarlo/proprietary/hammerhead/system/media/bootanimation.zip:system/media/bootanimation.zip
+
+# new adreno drivers
+PRODUCT_COPY_FILES += \
+    vendor/rascarlo/proprietary/hammerhead/system/NewAdrenoDrivers/firmware/a330_pfp.fw:system/vendor/firmware/a330_pfp.fw \
+    vendor/rascarlo/proprietary/hammerhead/system/NewAdrenoDrivers/firmware/a330_pm4.fw:system/vendor/firmware/a330_pm4.fw \
+    vendor/rascarlo/proprietary/hammerhead/system/NewAdrenoDrivers/lib/egl/eglsubAndroid.so:system/vendor/lib/egl/eglsubAndroid.so \
+    vendor/rascarlo/proprietary/hammerhead/system/NewAdrenoDrivers/lib/egl/libEGL_adreno.so:system/vendor/lib/egl/libEGL_adreno.so \
+    vendor/rascarlo/proprietary/hammerhead/system/NewAdrenoDrivers/lib/egl/libGLESv1_CM_adreno.so:system/vendor/lib/egl/libGLESv1_CM_adreno.so \
+    vendor/rascarlo/proprietary/hammerhead/system/NewAdrenoDrivers/lib/egl/libGLESv2_adreno.so:system/vendor/lib/egl/libGLESv2_adreno.so \
+    vendor/rascarlo/proprietary/hammerhead/system/NewAdrenoDrivers/lib/egl/libplayback_adreno.so:system/vendor/lib/egl/libplayback_adreno.so \
+    vendor/rascarlo/proprietary/hammerhead/system/NewAdrenoDrivers/lib/egl/libq3dtools_adreno.so:system/vendor/lib/egl/libq3dtools_adreno.so \
+    vendor/rascarlo/proprietary/hammerhead/system/NewAdrenoDrivers/lib/libadreno_utils.so:system/vendor/lib/libadreno_utils.so \
+    vendor/rascarlo/proprietary/hammerhead/system/NewAdrenoDrivers/lib/libc2d30-a3xx.so:system/vendor/lib/libc2d30-a3xx.so \
+    vendor/rascarlo/proprietary/hammerhead/system/NewAdrenoDrivers/lib/libC2D2.so:system/vendor/lib/libC2D2.so \
+    vendor/rascarlo/proprietary/hammerhead/system/NewAdrenoDrivers/lib/libCB.so:system/vendor/lib/libCB.so \
+    vendor/rascarlo/proprietary/hammerhead/system/NewAdrenoDrivers/lib/libgsl.so:system/vendor/lib/libgsl.so \
+    vendor/rascarlo/proprietary/hammerhead/system/NewAdrenoDrivers/lib/libllvm-qcom.so:system/vendor/lib/libllvm-qcom.so \
+    vendor/rascarlo/proprietary/hammerhead/system/NewAdrenoDrivers/lib/libOpenCL.so:system/vendor/lib/libOpenCL.so \
+    vendor/rascarlo/proprietary/hammerhead/system/NewAdrenoDrivers/lib/librs_adreno.so:system/vendor/lib/librs_adreno.so \
+    vendor/rascarlo/proprietary/hammerhead/system/NewAdrenoDrivers/lib/librs_adreno_sha1.so:system/vendor/lib/librs_adreno_sha1.so \
+    vendor/rascarlo/proprietary/hammerhead/system/NewAdrenoDrivers/lib/libRSDriver_adreno.so:system/vendor/lib/libRSDriver_adreno.so \
+    vendor/rascarlo/proprietary/hammerhead/system/NewAdrenoDrivers/lib/libsc-a3xx.so:system/vendor/lib/libsc-a3xx.so
